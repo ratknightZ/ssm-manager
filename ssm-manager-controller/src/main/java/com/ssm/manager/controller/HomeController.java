5a61335e847fb4de500e3450877024cd3cfda2b2
D0V3K4QIV0Q0E69\Administrator <514218291@qq.com>
2018-04-09 02:32:48 +0800

    ssm + shiro(beetl视图解析器一直启用不了，泪~)

--- ssm-manager-controller/src/main/java/com/ssm/manager/controller/HomeController.java
+++ ssm-manager-controller/src/main/java/com/ssm/manager/controller/HomeController.java
@@ -1,24 +1,17 @@
 package com.ssm.manager.controller;
 
-
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     private Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @GetMapping(value = "/u")
     public String index(){
-        return "index";
+        return "/template/in";
     }
 }
