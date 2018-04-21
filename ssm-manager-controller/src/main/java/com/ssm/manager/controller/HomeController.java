b64b78024189991c9a81da4fd3785acfc4a12225
D0V3K4QIV0Q0E69\Administrator <514218291@qq.com>
2018-04-22 07:37:20 +0800

    beetl

--- ssm-manager-controller/src/main/java/com/ssm/manager/controller/HomeController.java
+++ ssm-manager-controller/src/main/java/com/ssm/manager/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -11,7 +12,9 @@ public class HomeController {
     private Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping(value = "/u")
-    public String index(){
-        return "/template/in";
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("template/index");
+        mv.addObject("a","hello beetl!");
+        return mv;
     }
 }
