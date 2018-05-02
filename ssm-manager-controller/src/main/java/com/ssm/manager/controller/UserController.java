b19d55d5fd602951c969f749f7388df5808b4269
D0V3K4QIV0Q0E69\Administrator <514218291@qq.com>
2018-05-02 20:47:51 +0800

    ehcache

--- ssm-manager-controller/src/main/java/com/ssm/manager/controller/UserController.java
+++ ssm-manager-controller/src/main/java/com/ssm/manager/controller/UserController.java
@@ -1,19 +1,17 @@
 package com.ssm.manager.controller;
 
+import com.ssm.commons.exception.vo.AppResultVO;
 import com.ssm.manager.pojo.UserLogin;
 import com.ssm.manager.service.UserService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
@@ -21,12 +19,14 @@ public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    @ResponseBody
     @GetMapping("/user/get_login_user")
-    public String getLoginUser(){
+    public AppResultVO getLoginUser(){
         Subject currentUser = SecurityUtils.getSubject();
         String userName = (String) currentUser.getPrincipal();
         UserLogin userLogin = userService.getLoginInfo(userName);
-        return userLogin.getUserName();
+        AppResultVO resultVO = new AppResultVO();
+        resultVO.setIsSuccess(1);
+        resultVO.setData(userLogin);
+        return resultVO;
     }
 }
